8299025921d7abf4f896fe0b12ddb2eeb7ffed0b
alice8347 <alice8347@hotmail.com>
2015-08-28 09:22:46 -0400

    First Commit

---
+++ ScannerAssi/src/ScannerAssignment.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ScannerAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int integer = sc.nextInt();
+		System.out.println(integer);
+	}
+
+}
